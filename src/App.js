b8b08f0df882f2c7ea889aef45640d6fd9395740
nhannafi <nour.hannafi@engie.com>
2020-11-17 16:07:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import React, {Component} from 'React';
+import { render } from "@testing-library/react";
+
+
+class App extends Component {
+  constructor(props) {
+    super(propos)
+  }
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+export default App;
